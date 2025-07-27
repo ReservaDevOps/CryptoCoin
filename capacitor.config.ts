2627370db341a85e8f1e893ded1f9d216866440b
ReservaDevOps <reservadevops@gmail.com>
2025-07-27 20:23:12 -0300

    Inital Commit

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'com.seuapp.cryptocoin',
   appName: 'CryptoCoin',
   webDir: 'www'
 };
